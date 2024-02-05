08d56f2f757c602df17ee51f27bd9b6f2f4402bc
Alejandra Colcha <da.colcha@hotmail.com>
2024-02-05 13:53:39 -0500

    Creación clase Triangle

---
+++ src/org/triangles/Triangle.java
@@ -0,0 +1,22 @@
+package org.triangles;
+
+public class Triangle {
+    private double lado1;
+    private double lado2;
+    private double lado3;
+    private String type;
+
+    public Triangle(double lado1, double lado2, double lado3) {
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+        this.lado3 = lado3;
+    }
+
+    public void getType() {
+
+    }
+
+    public double getArea() {
+        return null;
+    }
+}
