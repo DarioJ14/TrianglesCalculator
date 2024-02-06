d13f2456455c41a4689d10c50820b966a21ed308
DarÃío Charro <dario14julian@gmail.com>
2024-02-06 08:30:37 -0500

    Adición del método para identificar el tipo de lado de un trriángulo según sus lados

--- src/main/java/org/triangles/Triangle.java
+++ src/main/java/org/triangles/Triangle.java
@@ -4,16 +4,23 @@ public class Triangle {
     private double lado1;
     private double lado2;
     private double lado3;
-    private String type;
+    private String typeAccordingSide;
 
     public Triangle(double lado1, double lado2, double lado3) {
         this.lado1 = lado1;
         this.lado2 = lado2;
         this.lado3 = lado3;
+        this.typeAccordingSide = getType();
     }
 
-    public void getType() {
-
+    public String getType() {
+        if (this.lado1 == this.lado2 && this.lado2 == this.lado3) {
+            return "Equilátero";
+        } else if (this.lado1 == this.lado2 || this.lado2 == this.lado3 || this.lado1 == this.lado3) {
+            return "Isósceles";
+        } else {
+            return "Escaleno";
+        }
     }
 
     public double getArea() {
